66ef54a211f12d7d05ceecda404f80e09486da3a
Davi Monteiro <montsgit@gmail.com>
2025-08-17 01:48:02 -0300

    fix: gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,9 +23,7 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.book_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdkVersion(24)
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
