caef466061eecb5a48f6fd65e91e96610a90174a
Davi Monteiro <montsgit@gmail.com>
2025-08-19 10:48:28 -0300

    feat: books per user

--- lib/features/details/logic/provider/delete_book_provider.dart
+++ lib/features/details/logic/provider/delete_book_provider.dart
@@ -1,9 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class DeleteBookProvider extends ChangeNotifier {
-  Future<void> deleteBookFromFirestore({required String bookId}) async {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  Future<void> deleteBookFromFirestore({
+    required String bookId,
+  }) async {
+    final userId = firebaseAuth.currentUser!.uid;
     await FirebaseFirestore.instance
+        .collection(
+          'users',
+        )
+        .doc(
+          userId,
+        )
         .collection(
           'books',
         )
--- lib/features/register_book/logic/provider/register_book_provider.dart
+++ lib/features/register_book/logic/provider/register_book_provider.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterBookProvider extends ChangeNotifier {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
   Future<void> sendBookToFirestore({
     //TODO: atualPage
     required pages,
@@ -12,18 +15,42 @@ class RegisterBookProvider extends ChangeNotifier {
     required format,
     required synopsis,
   }) async {
-    await FirebaseFirestore.instance.collection('books').add({
-      'pages': pages,
-      'publicationDate': publicationDate,
-      'title': title,
-      'author': author,
-      'gender': gender,
-      'format': format,
-      'synopsis': synopsis,
-    });
+    final userId = firebaseAuth.currentUser!.uid;
+    await
+    //F
+    firebaseFirestore //.instance
+        .collection(
+          'users',
+        )
+        .doc(userId)
+        .collection(
+          'books',
+        )
+        .add({
+          'pages': pages,
+          'publicationDate': publicationDate,
+          'title': title,
+          'author': author,
+          'gender': gender,
+          'format': format,
+          'synopsis': synopsis,
+        });
   }
 
   Stream<QuerySnapshot> showBooksFromFirestore() {
-    return FirebaseFirestore.instance.collection('books').snapshots();
+    final userId = firebaseAuth.currentUser!.uid;
+    return
+    //F
+    firebaseFirestore //.instance
+        .collection(
+          'users',
+        )
+        .doc(
+          userId,
+        )
+        .collection(
+          'books',
+        )
+        .snapshots();
   }
 }
