b62d3abe9404d9d2af9df4b27d7c4757ecc3aea4
Davi Monteiro <montsgit@gmail.com>
2025-08-27 20:33:36 -0300

    cleanup: auth provider

--- lib/features/auth/logic/providers/auth_provider.dart
+++ lib/features/auth/logic/providers/auth_provider.dart
@@ -1,4 +1,3 @@
-import 'package:book_app/common/constants/app_strings.dart';
 import 'package:book_app/core/exceptions/app_exceptions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
